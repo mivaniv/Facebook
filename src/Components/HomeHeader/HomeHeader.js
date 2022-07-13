d0ede18c3de75af90b6cf2af4511b747d86ab87e
Andrii Kondysiuk <kondysiuka@gmail.com>
2022-07-13 18:15:37 +0300

    header upd2

--- src/Components/HomeHeader/HomeHeader.js
+++ src/Components/HomeHeader/HomeHeader.js
@@ -12,10 +12,16 @@ import NotificationsIcon from "@material-ui/icons/Notifications";
 import OfflineBoltIcon from "@material-ui/icons/OfflineBolt";
 import DialpadIcon from "@material-ui/icons/Dialpad";
 import HomeIcon from "@material-ui/icons/Home";
+
+
 import PeopleIcon from "@material-ui/icons/People";
 import SmartDisplayIcon from "@mui/icons-material/SmartDisplay";
 import AutoAwesomeMosaicIcon from "@mui/icons-material/AutoAwesomeMosaic";
 import mark from "./images/mark.jpeg";
+
+
+
+
 
 function FormAuth({ user }) {
   const navigate = useNavigate();
