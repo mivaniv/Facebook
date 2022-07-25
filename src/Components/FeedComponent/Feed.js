a4f6f06be5fcd79b45ee5274a0a6898db32302fc
MaxLuzhetsky <lujetsky.max@gmail.com>
2022-07-25 18:40:40 +0300

    created post component and fixed right column and center styles

--- src/Components/FeedComponent/Feed.js
+++ src/Components/FeedComponent/Feed.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Story from "./Story/Story"
 import StoryBlock from "./StoryBlock/StoryBlock"
 import "./Feed.css"
 import PostInput from './Input/PostInput';
 
+import Post from './Post/Post'
+
+import { collection, onSnapshot } from 'firebase/firestore';
+
 export default function Feed() {
+    const [posts , setPosts] = useState([]);
+
+   
+
+   
     return (
         <div className='feed'>
             <div className='feed-container'>
 
                 <StoryBlock></StoryBlock>
                 <PostInput></PostInput>
+                <Post 
+                   profPic="https://avatars.githubusercontent.com/u/1?v=4"
+                   nickName="mojombo"
+                   recTime="17:55"
+                   text="Hello world"
+                   postPic="https://avatars.githubusercontent.com/u/1?v=4"
+
+
+
+
+                       />
             </div>
         </div>
     )
---
+++ src/Components/FeedComponent/Post/Post.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Post.css'
+
+export default function Post({ profPic, nickName, recTime, text, postPic }) {
+    return (
+        <div>
+            <div className='post-container'>
+                <div className='post-top-section'>
+                    <img className='post-profile-pic' src={profPic}></img>
+                    <div>
+
+                        <p className='post-user-nickname'>{nickName}</p>
+                        <p className='post-time'>{recTime}</p>
+                    </div>
+
+                </div>
+                <div className='post-content'>
+                    <p className='post-content-text'>{text}</p>
+                    <div className='post-content-pic-containter'>
+                        <img className='post-content-pic' src={postPic}></img>
+                    </div>
+                </div>
+                <div>
+                    <button>Like</button>
+                    <button>Share</button>
+                </div>
+            </div>
+
+        </div >
+    )
+}
